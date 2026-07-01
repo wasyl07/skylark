5aa55c496e80ba16d37ba3881561882bb9290171
Adam Wasilewski <Adam_Wasilewski_99@proton.me>
2026-07-01 20:21:57 +0200

    Add PRU toolchain and pru firmware

---
+++ docker/pru_0_firmware/gen/PRU_gpioToggle.pp
@@ -0,0 +1,4 @@
+gen/PRU_gpioToggle.object: PRU_gpioToggle.c
+gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/stdint.h
+gen/PRU_gpioToggle.object: /usr/share/ti/cgt-pru/include/_stdint40.h
+gen/PRU_gpioToggle.object: include/am335x/pru_cfg.h
